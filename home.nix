b6e356d5be68d5ab60a5c2bbb6ee73de1c858a75
Chandrashekar M <ironavenger10@gmail.com>
2025-08-13 21:10:32 +0530

    made dir for future modularity

--- home.nix
+++ home.nix
@@ -38,7 +38,8 @@
 
   # Home Manager is pretty good at managing dotfiles. The primary way to manage
   # plain files is through 'home.file'.
-  home.file = {
+  
+  #home.file = {
     # # Building this configuration will create a copy of 'dotfiles/screenrc' in
     # # the Nix store. Activating the configuration will then make '~/.screenrc' a
     # # symlink to the Nix store copy.
@@ -49,8 +50,9 @@
     #   org.gradle.console=verbose
     #   org.gradle.daemon.idletimeout=3600000
     # '';
-  };
+  #};
 
+  home.file."/boot/EFI/refind/refind.conf".source =  ./hardcoded/refind.conf;
   # Home Manager can also manage your environment variables through
   # 'home.sessionVariables'. These will be explicitly sourced when using a
   # shell provided by Home Manager. If you don't want to manage your shell
