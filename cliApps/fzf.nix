9ac282c42e8f05c9157d8bff1b9e2943d15b603f
Chandrashekar M <ironavenger10@gmail.com>
2025-08-14 21:31:09 +0530

    configured fzf and its exclusions very nicely

--- cliApps/fzf.nix
+++ cliApps/fzf.nix
@@ -8,7 +8,9 @@
 		enableBashIntegration = true;
 		enableFishIntegration = true;
 		tmux.enableShellIntegration = true;
+ 		#fileWidgetCommand = "fd --type f . '/'";
 
+		fileWidgetCommand = "fd -F --hidden . '/' --exclude /proc --exclude /sys --exclude /dev --exclude /run --exclude /nix/store --exclude ~/.nix-defexpr --exclude ~/.cache --exclude ~/.local/share/Trash --exclude /var";
 
 
 	};
