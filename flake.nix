cf5e5b6c768ae6ac29b983e5acaabaf120aa8e0e
Chandrashekar M <ironavenger10@gmail.com>
2025-08-16 21:27:10 +0530

    added plasms-manager to flake.niux

--- flake.nix
+++ flake.nix
@@ -6,14 +6,20 @@
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
     
     zen-browser = {
-	url = "github:0xc000022070/zen-browser-flake";
-	inputs.nixpkgs.follows = "nixpkgs";
+    	url = "github:0xc000022070/zen-browser-flake";
+    	inputs.nixpkgs.follows = "nixpkgs";
     };
 
     home-manager = {
-	url = "github:nix-community/home-manager";
-	inputs.nixpkgs.follows = "nixpkgs";
+    	url = "github:nix-community/home-manager";
+    	inputs.nixpkgs.follows = "nixpkgs";
 
+    };
+
+    plasma-manager = {
+      url = "github:nix-community/plasma-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.home-manager.follows = "home-manager";
     };
   };
 
